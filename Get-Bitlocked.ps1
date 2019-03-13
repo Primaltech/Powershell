77943dc933076e8fe9b24a1fb3f3078a8abead9c
Primaltech <17487594+Primaltech@users.noreply.github.com>
2019-03-13 09:38:05 -0400

    Create Get-Bitlocked.ps1

---
+++ Get-Bitlocked.ps1
@@ -0,0 +1,5 @@
+﻿#get a list of bitlocked computers on the domain with recovery password and creation time
+#Run this script as a one-liner
+
+
+Get-AdObject -Filter "objectclass -eq 'msFVE-RecoveryInformation'" -Properties DistinguishedName, msFVE-RecoveryPassword, WhenCreated | Select-Object -Property @{n="ComputerName";e={$_.DistinguishedName.Split(',',2)[1]}}, msFVE-RecoveryPassword , WhenCreated
